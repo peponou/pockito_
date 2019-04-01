07174d691dabbce6e98bc1561bf55536d353d589
Dimitra <georgioudolfidimitra@gmail.com>
2019-04-02 00:50:30 +0300

    delete constants file

--- server/src/main/java/com/urlshortener/pockito/utils/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.urlshortener.pockito.utils;
-
-public interface Constants {
-    static final String GET_URL_BY_SHORTID = "/getUrl/{shortId}";
-    static final String SAVE_URL = "/saveUrl";
-}
